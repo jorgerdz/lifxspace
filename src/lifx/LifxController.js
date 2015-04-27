6abc6e2b8c863cc6291b8c0281c399b98ef8478a
Jorge Rodriguez <mail@jorgerdz.com>
2015-04-27 04:48:41 -0500

    Scene corrections, selectors and labels, removing bower_components/ from repo

---
+++ src/lifx/LifxController.js
@@ -0,0 +1,163 @@
+(function(){
+
+  angular
+       .module('lifx', ['LocalStorageModule'])
+       .controller('LifxController', [
+          '$scope', 'lifxService', '$mdSidenav', '$mdDialog', '$log', '$q', '$interval', 'localStorageService',
+          LifxController
+       ]);
+
+  function LifxController($scope, lifxService, $mdSidenav, $mdDialog, $log, $q, $interval, localStorageService) {
+
+    var sceneInterval,
+        intervalHelper = 0;
+
+    $scope.scenes = [
+              {color: ['3F7CAC', '95AFBA', 'BDC4A7', 'D5E1A3', 'E2F89C']},
+              {color: ['FA8334', 'FFFD77', 'FFE882', '388697', '271033']},
+              {color: ['65ADF5', 'A08FF3', 'D8CFF4', 'CBABBD', 'AC8985']},
+              {color: ['FFC09F', 'FFEE93', 'FCF5C7', 'A0CED9', 'ADF7B6']},
+              {color: ['82C187', '92D74F', 'B4EF5D', '46584F', '211916']},
+              {color: ['33CBE5', '3AA2FF', '4BFFEF', '36EAAA', '3CFE82']}
+            ];
+
+    $scope.showDialog = function($event) {
+       var parentEl = angular.element(document.body);
+
+       var html = '<md-dialog aria-label="List dialog">' +
+               '  <md-content>'+
+               '    <md-list>'+
+               '      <md-item>'+
+               '       <p>Please enter your token generated at <a href="http://cloud.lifx.com">cloud.lifx.com</a></p>' +
+               '      </md-item>'+
+               '      <md-item>'+
+               '      <md-input-container flex><label>Token</label><input type="text" ng-model="token"></md-input-container>'+
+               '      </md-item>'+
+               '      <md-item ng-show="error && !verified">'+
+               '       <p>We couldn\'t verify your information, please provide a correct token.</p>' +
+               '      </md-item>'+
+               '      <md-item ng-show="verified">'+
+               '       <p>Success!</p>' +
+               '      </md-item>'+
+               '    </md-list>'+
+               '  </md-content>' +
+               '  <div class="md-actions">' +
+               '    <md-button ng-hide="verified" aria-label="Verify" class="md-raised md-primary" ng-click="verify()">' +
+               '      Verify' +
+               '    </md-button>' +
+               '    <md-button ng-show="verified" aria-label="Cloase dialog" class="md-raised md-warn" ng-click="closeDialog()">' +
+               '      Close' +
+               '    </md-button>' +
+               '  </div>' +
+               '</md-dialog>';
+       var token = '';
+       $mdDialog.show({
+         parent: parentEl,
+         targetEvent: $event,
+         template: html,
+         locals: {
+           token : token
+         },
+         controller: DialogController
+      });
+
+      function DialogController(scope, $mdDialog, token, lifxService, localStorageService) {
+        scope.token = token;
+        scope.closeDialog = function() {
+          $mdDialog.hide();
+          listLights();
+        };
+        scope.verify = function(){
+          lifxService.verify(scope.token).success(function(data){
+            scope.verified = true;
+          }).error(function(err){
+            scope.error = true;
+          });
+        };
+        scope.setToken = function(token){
+          localStorageService.set('token', token);
+        };
+      }
+    }
+
+    $scope.turnOn = function(){
+      $scope.stopScene();
+      lifxService.on();
+    };
+
+    $scope.turnOff = function(){
+      $scope.stopScene();
+      lifxService.off();
+    };
+
+    $scope.setRandomColor = function() {
+      $scope.stopScene();
+      lifxService.setRandomLights();
+    };
+
+    $scope.setScene = function(scene){
+      if(scene.selected){
+        scene.selected = false;
+        $scope.stopScene();
+      }
+      else{
+        $scope.scenes.forEach(function(data){
+          data.selected = false;
+        });
+        scene.selected = true;
+
+        $scope.selectedScene = scene.color;
+        interval();
+        sceneInterval = $interval(interval, 10*1000);
+      }
+    };
+
+    function interval(){
+      lifxService.setColorForScene($scope.selectedScene[intervalHelper]);
+      intervalHelper++;
+      if(intervalHelper==$scope.selectedScene.length-2)
+        intervalHelper=0;
+    }
+
+    $scope.stopScene = function() {
+      if (angular.isDefined(sceneInterval)) {
+        $interval.cancel(sceneInterval);
+        stop = undefined;
+      }
+    };
+
+    function listLights(){
+      lifxService
+            .listLights()
+            .then( function( data ) {
+              $scope.color = {};
+              $scope.color.brightness = data[1].brightness;
+              $scope.color.hue = data[1].color.hue;
+              $scope.color.saturation = data[1].color.saturation;
+              $scope.color.kelvin = data[1].color.kelvin;
+              $scope.lights = data;
+              $scope.selected = $scope.lights[1];
+              lifxService.setSelector($scope.selected);
+            });
+    }
+
+    if(localStorageService.get('token')){
+      listLights();
+    } else {
+      $scope.showDialog();
+    }
+
+    $scope.$watchGroup(['color.hue', 'color.saturation', 'color.brightness', 'color.kelvin'], function(){
+      if($scope.color) {
+        $scope.stopScene();
+        lifxService.setHSBK($scope.color);
+      }
+    });
+
+    $scope.selectLight  = function( light ) {
+      $scope.selected = light;
+      lifxService.setSelector($scope.selected);
+    }
+  }
+
+})();
